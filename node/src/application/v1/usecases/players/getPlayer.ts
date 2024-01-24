6b8eb38d241c6b8b1d8e065d0c205338e2479718
Matheus Brant <matheusbrantgo@gmail.com>
2024-01-24 03:21:13 +0000

    chore: start build query filter

--- node/src/application/v1/usecases/players/getPlayer.ts
+++ node/src/application/v1/usecases/players/getPlayer.ts
@@ -1,6 +1,9 @@
+import { base_url } from "../../../../config/api";
+import { buildQueryString } from "../../../../helpers/buildQueryString";
+
 export async function teste() {
-  const url = "https://drop-api.ea.com/rating/fc-24?limit=1";
-  const response = await fetch(url);  
+  const queryString = buildQueryString({ search: 'Yedder' })
+  const response = await fetch(`${base_url}${queryString}`);  
   const responseJson = await response.json();
-  console.log(JSON.stringify(responseJson.items));
+  console.log(responseJson.items[0].lastName);
 }
--- node/src/index.ts
+++ node/src/index.ts
@@ -1,11 +1,7 @@
 import { teste } from './application/v1/usecases/players/getPlayer';
 
 async function run() {
-  const greeting: string = 'Hello, TypeScript!';
-  console.log(greeting);
-
-  const result = await teste();
-  console.log(result);
+  await teste();
 }
 
 run();
