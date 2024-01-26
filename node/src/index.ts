d4b555fd3d474d64649fd7193123f4352f5aa54d
Matheus Brant <matheusbrantgo@gmail.com>
2024-01-26 02:10:27 +0000

    chore: mapping positions

--- node/src/index.ts
+++ node/src/index.ts
@@ -1,7 +1,7 @@
-import { getNationalities } from './application/v1/usecases/players/getNationalities'
+import { getPositions } from './application/v1/usecases/players/getPositions'
 
 async function run (): Promise<void> {
-  await getNationalities()
+  await getPositions()
 }
 
 void run()
