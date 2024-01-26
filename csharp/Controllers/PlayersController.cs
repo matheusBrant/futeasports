fd472eea5a53e583c742272ffafea1b13849156c
MiltonPereiraBH <milton.pereira@nexaas.com>
2024-01-26 17:47:06 -0300

    init project csharp

---
+++ csharp/Controllers/PlayersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+[ApiController]
+[Route("api/players")]
+public class PlayersController : ControllerBase
+{
+    private readonly EAFc24ApiClient _eaFc24ApiClient;
+
+    public PlayersController(EAFc24ApiClient eaFc24ApiClient)
+    {
+        _eaFc24ApiClient = eaFc24ApiClient;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetPlayers()
+    {
+        var players = await _eaFc24ApiClient.GetPlayers();
+
+        if (players != null)
+        {
+            return Ok(players);
+        }
+
+        return StatusCode(500, "Erro ao obter jogadores da API da EA FC 24");
+    }
+}
---
+++ csharp/Domain/Model/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Player
+{
+    public int Rank { get; set; }
+    public int OverallRating { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string CommonName { get; set; }
+    public DateTime Birthdate { get; set; }
+    public int Height { get; set; }
+    public int SkillMoves { get; set; }
+    public int WeakFootAbility { get; set; }
+    public int AttackingWorkRate { get; set; }
+    public int DefensiveWorkRate { get; set; }
+    public int PreferredFoot { get; set; }
+    public string LeagueName { get; set; }
+    public int Weight { get; set; }
+    public string AvatarUrl { get; set; }
+    public string ShieldUrl { get; set; }
+    public Dictionary<string, object> Relationships { get; set; }
+    public Dictionary<string, object> Attributes { get; set; }
+}
