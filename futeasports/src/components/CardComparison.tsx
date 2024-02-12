0834506b7124323275cb2c8ba43e5def4a73b5a9
Matheus Brant <matheusbrantgo@gmail.com>
2024-02-12 01:59:31 +0000

    feat: adding loading and fix styles

--- futeasports/src/components/CardComparison.tsx
+++ futeasports/src/components/CardComparison.tsx
@@ -4,13 +4,13 @@ export const CardComparison = () => {
   return (
     <div className="mx-auto max-w-screen-lg px-4">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <div className="md:p-10 flex items-center justify-center">
+        <div className="sm:p-10 md:p-10 flex items-center justify-center">
           <CardPlayer />
         </div>
         <div className="md:p-5 flex items-center justify-center">
-          <h1 className="font-mono text-5xl text-lime-950">X</h1>
+          <h1 className="font-mono text-5xl text-lime-950">VS</h1>
         </div>
-        <div className="md:p-10 flex items-center justify-center">
+        <div className="sm:p-10 md:p-10 flex items-center justify-center">
           <CardPlayer />
         </div>
       </div>
